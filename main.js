af37e853f9355b7106561198bb9bf0f6484d2222
danielkldron <dc7027233@gmail.com>
2023-02-08 22:48:08 -0500

    ya cierra el menu al dar click

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 const iconMenu = document.querySelector(".bx-menu");
-const menu = document.querySelector(".menu")
+const menu = document.querySelector(".menu");
 
-iconMenu.addEventListener("click", function() {
-    console.log(menu.classList.toggle("menu-show"));
-});
+function closeMenu() {
+    menu.classList.toggle("menu-show");
+}
+
+iconMenu.addEventListener("click", closeMenu);
+
+const contentMenu = document.querySelectorAll(".menu li");
+
+for (const li of contentMenu) {
+    li.addEventListener("click", closeMenu);
+}
